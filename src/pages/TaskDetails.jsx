bc8419cc41d91b37ad5adf07a9a1365d4438e026
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-05-13 09:29:27 +0100

    fixed a bug

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -34,7 +34,7 @@ const TaskDetails = () => {
       if (difference > 0) {
         timeLeft = {
           days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-          hours: Math.floor(difference / (1000 * 60 * 60 * 24) / (1000 * 60 * 60)),
+          hours: Math.floor(difference % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)),
           minutes: Math.floor((difference / 1000 / 60) % 60),
           seconds: Math.floor((difference / 1000) % 60),
         };
